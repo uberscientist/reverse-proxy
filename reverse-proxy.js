bcbd8cb35b6d5c074aaef61875d0cc76e2fecda1
Christopher Toledo <simoon@gmx.com>
2012-06-16 19:50:48 +0400

    new VPS

--- reverse-proxy.js
+++ reverse-proxy.js
@@ -3,8 +3,28 @@ var httpProxy = require('http-proxy');
 var options = {
   hostnameOnly: true,
   router: {
+    'www.hivego.info': '127.0.0.1:3001',
     'hivego.info': '127.0.0.1:3001',
-    'wetwarehacks.com': '127.0.0.1:3002'
+    'long.hivego.info': '127.0.0.1:3002',
+    'wetwarehacks.com': '127.0.0.1:8080',
+    'www.wetwarehacks.com': '127.0.0.1:8080',
+    'www.bocksystems.com': '127.0.0.1:8080',
+    'bocksystems.com': '127.0.0.1:8080',
+    'charlesbock.com': '127.0.0.1:8080',
+    'www.charlesbock.com': '127.0.0.1:8080',
+    'fgsfds.com': '127.0.0.1:8080',
+    'www.fgsfds.com': '127.0.0.1:8080',
+    'ihacku.org': '127.0.0.1:8080',
+    'www.ihacku.org': '127.0.0.1:8080',
+    'ihacku.net': '127.0.0.1:8080',
+    'www.verballistics.com': '127.0.0.1:8080',
+    'verballistics.com': '127.0.0.1:8080',
+    'www.blastwavelabs.com': '127.0.0.1:8080',
+    'blastwavelabs.com': '127.0.0.1:8080',
+    'mindsforge.com': '127.0.0.1:8080',
+    'www.mindsforge.com': '127.0.0.1:8080',
+    'abovesobelow.com': '127.0.0.1:8080',
+    'www.abovesobelow.com': '127.0.0.1:8080'
   }
 }
 
